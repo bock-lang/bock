235f98aa543f4f02ef23eab65542fc846d3bb3a2
Bock Scaffold <scaffold@bocklang.org>
2026-05-07 00:23:49 -0400

    Update stro config

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://bocklang.org',
   output: 'static',
   trailingSlash: 'never',
+
   build: {
     format: 'directory',
   },
+
+  adapter: cloudflare(),
 });
